204817514061e0211c9976882fa0fcaa7263571b
Prajapati777 <pmayank325@gmail.com>
2026-05-01 12:48:42 +0000

    day20

--- client/src/app/supplylink/components/product/product.component.ts
+++ client/src/app/supplylink/components/product/product.component.ts
@@ -1,4 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-export class ProductComponent {
- 
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+
+  productForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.productForm = this.fb.group({
+      productId: [''],
+      warehouseId: ['', [Validators.required, Validators.min(1)]],
+      productName: ['', Validators.required],
+      productDescription: [''],
+      quantity: ['', [Validators.required, Validators.min(0)]],
+      price: ['', [Validators.required, Validators.min(1)]]
+    });
+  }
+
+  get productId() {
+    return this.productForm.get('productId');
+  }
+
+  get warehouseId() {
+    return this.productForm.get('warehouseId');
+  }
+
+  get productName() {
+    return this.productForm.get('productName');
+  }
+
+  get productDescription() {
+    return this.productForm.get('productDescription');
+  }
+
+  get quantity() {
+    return this.productForm.get('quantity');
+  }
+
+  get price() {
+    return this.productForm.get('price');
+  }
+
+  onSubmit(): void {
+    if (this.productForm.valid) {
+      console.log('Product Form Data:', this.productForm.value);
+      alert('Product details submitted successfully!');
+    } else {
+      this.productForm.markAllAsTouched();
+    }
+  }
 }
--- client/src/app/supplylink/components/warehouse/warehouse.component.ts
+++ client/src/app/supplylink/components/warehouse/warehouse.component.ts
@@ -1,5 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-export class WarehouseComponent {
- 
+@Component({
+  selector: 'app-warehouse',
+  templateUrl: './warehouse.component.html',
+  styleUrls: ['./warehouse.component.scss']
+})
+export class WarehouseComponent implements OnInit {
 
+  warehouseForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.warehouseForm = this.fb.group({
+      warehouseId: [''],
+      supplierId: ['', [Validators.required, Validators.min(1)]],
+      warehouseName: ['', Validators.required],
+      location: [''],
+      capacity: ['', [Validators.required, Validators.min(0)]]
+    });
+  }
+
+  get warehouseId() {
+    return this.warehouseForm.get('warehouseId');
+  }
+
+  get supplierId() {
+    return this.warehouseForm.get('supplierId');
+  }
+
+  get warehouseName() {
+    return this.warehouseForm.get('warehouseName');
+  }
+
+  get location() {
+    return this.warehouseForm.get('location');
+  }
+
+  get capacity() {
+    return this.warehouseForm.get('capacity');
+  }
+
+  onSubmit(): void {
+    if (this.warehouseForm.valid) {
+      console.log('Warehouse Form Data:', this.warehouseForm.value);
+      alert('Warehouse details submitted successfully!');
+    } else {
+      this.warehouseForm.markAllAsTouched();
+    }
+  }
 }
